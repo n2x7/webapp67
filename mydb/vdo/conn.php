71b99ea141b6a0b38c913fb99c6f245661412d6d
n2x7 <129843445+n2x7@users.noreply.github.com>
2024-09-26 15:38:29 +0700

    Delete mydb directory

--- mydb/vdo/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli('localhost','root','','vdo');
-$conn->query("SET NAMES utf8");
-if($conn->connect_error){
-    die("Connection Fail God damn it ". $conn->$conn_error);
-}
-
-?>
